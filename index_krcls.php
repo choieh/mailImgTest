cea52bb886da50a99f9beb1c507f8a8dc5e37f46
choieh <bobomi66@naver.com>
2025-07-03 11:21:59 +0900

    이메일 서명

---
+++ index_krcls.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <table cellpadding="0" cellspacing="0" border="0"
+        style=" font-family:'Malgun Gothic','Apple SD Gothic Neo','Segoe UI',sans-serif; font-size:14px; color:#000;width:903px; height: 248px; padding-top: 33px; background-image:url('https://modulearning.kr/mailimg/bg_mail_moduGroup.jpg'); background-size:903px 248px; background-repeat:no-repeat; line-height:1.6;">
+        <tbody>
+            <tr>
+                <td style="vertical-align: top; width:433px;">
+                    <!-- 로고 자리에 필요 없으면 빈 칸 유지 -->
+                </td>
+                <td style="vertical-align: top;">
+                    <table cellpadding="0" cellspacing="0" border="0">
+                        <!-- 이름 -->
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div style="display: flex; align-items: flex-end;">
+                                        <span style="font-size:28px; font-weight:bold; color:#000;">박 광 훈</span>
+                                        <span
+                                            style="display:inline-block; font-size:10px; font-weight:bold; color:#000; margin-left:8px;padding-bottom:2px; letter-spacing: -0.1em;">HRD영업부
+                                            / 부문장</span>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <!-- 이메일, 주소 -->
+                            <tr>
+                                <td>
+                                    <div
+                                        style="display: flex; justify-content: flex-start; align-items: flex-end; gap: 15px; margin-top: 6px;">
+                                        <span style="font-size: 9px; color:#f7931e; font-weight:bold;">E‑mail</span>
+                                        <a href="mailto:learning-park@modulearning.kr"
+                                            style="font-size: 10px; color:#000; text-decoration:none;">learning-park@modulearning.kr</a>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div
+                                        style="display: flex; justify-content: flex-start; align-items: flex-end; gap: 6px;">
+                                        <span style=" font-size: 9px; color:#f7931e; font-weight:bold;">Address</span>
+                                        <span style="font-size: 10px; letter-spacing: -0.12em;">금천구 가산디지털1로 75-15
+                                            하우스디와이즈타워 612호</span>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <!-- 전화번호 -->
+                            <tr>
+                                <td>
+                                    <div
+                                        style="display: flex;justify-content: flex-start;align-items: flex-end;gap: 70px;margin-top: 10px;">
+                                        <span style="font-size: 9px; color:#f7931e; font-weight:bold;">Tel</span>
+                                        <span style="font-size: 9px; color:#f7931e; font-weight:bold;">Fax</span>
+                                    </div>
+                                </td>
+
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div
+                                        style="display: flex;justify-content: flex-start;align-items: flex-start;gap: 14px;">
+                                        <div>
+                                            <span
+                                                style="display:block; font-size: 10px; color:#000000; letter-spacing: 0.02em;">070.4740.4941</span>
+                                            <span
+                                                style="display:block; margin-top: -2px; font-size: 10px; color:#000000; letter-spacing: 0.02em;">010.3333.4444</span>
+                                        </div>
+                                        <span
+                                            style="font-size: 10px; color:#000000; letter-spacing: 0.02em;">02.6956.8622</span>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <!-- SNS -->
+                            <tr>
+                                <td>
+                                    <div
+                                        style="display: flex;justify-content: flex-start;align-items: flex-end;gap: 7px;margin-top: 10px;">
+                                        <a href="https://www.instagram.com/ekclsofficial/" target="_blank"
+                                            style="display:inline-block; width:26px; height:26px;">
+                                            <img src="https://modulearning.kr/mailimg/ico_insta@2x.png" alt="Instagram"
+                                                width="26" height="26" style="border:0;">
+                                        </a>
+                                        <a href="https://facebook.com" target="_blank"
+                                            style="display:inline-block; width:26px; height:26px;">
+                                            <img src="https://modulearning.kr/mailimg/ico_facebook@2x.png"
+                                                alt="Facebook" width="26" height="26" style="border:0;">
+                                        </a>
+                                        <a href="https://blog.naver.com" target="_blank"
+                                            style="display:inline-block; width:26px; height:26px;">
+                                            <img src="https://modulearning.kr/mailimg/ico_blog@2x.png" alt="Blog"
+                                                width="26" height="26" style="border:0;">
+                                        </a>
+                                        <a href="https://youtube.com" target="_blank"
+                                            style="display:inline-block; width:26px; height:26px;">
+                                            <img src="https://modulearning.kr/mailimg/ico_youtube@2x.png" alt="YouTube"
+                                                width="26" height="26" style="border:0;">
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
